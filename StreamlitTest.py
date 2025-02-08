d23019a9ca4252903f3aa2538d8eadc97f31aa75
Luoluo-sxx <71624473+Luoluo-sxx@users.noreply.github.com>
2025-02-08 17:53:12 +0800

    Update StreamlitTest.py

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -3,10 +3,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write("hello world!")
-df = pd.DataFrame(
-    np.random.randn(10, 5),
-    columns=('第%d列' % (i+1) for i in range(5))
-)
+# st.write("hello world!")
+# df = pd.DataFrame(
+#     np.random.randn(10, 5),
+#     columns=('第%d列' % (i+1) for i in range(5))
+# )
+# st.table(df)
+print('Hello World!')
 
-st.table(df)
