03833eb5266932c94b03b8787b96be8a5d44e004
Luoluo-sxx <71624473+Luoluo-sxx@users.noreply.github.com>
2025-02-12 16:53:14 +0800

    Update StreamlitTest.py

--- StreamlitTest.py
+++ StreamlitTest.py
@@ -1,10 +1,12 @@
+pip install Streamlit==1.42.0
 
 import streamlit as st
 st.write("hello world!")
+
+
 # df = pd.DataFrame(
 #     np.random.randn(10, 5),
 #     columns=('第%d列' % (i+1) for i in range(5))
 # )
-# st.table(df)
-# print('Hello World!')
+
 
